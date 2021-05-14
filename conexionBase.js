6b29c95934c04dd30d048f6e0212be49c114e325
Juan Esteban Jaramillo <juanesteban0607@live.com>
2021-05-14 11:10:37 -0500

    :C:/Program Files/Git/

--- conexionBase.js
+++ conexionBase.js
@@ -1,5 +1,15 @@
 const Sequelize = require('sequelize');
-const path = 'mysql://root@localhost:3306/delilah_resto'
+const db_data = require('db_connection_data');
+const Sequelize   = require('sequelize');
+const sequelize   = new Sequelize( db_data.conf_db_name, db_data.conf_user, db_data.conf_password, { 
+    host: db_data.conf_db_host,
+    dialect: 'mysql',
+    port: db_data.conf_port,
+    dialectOptions: {
+        multipleStatements: true
+    }
+});
+module.exports    = sequelize;
 const sequelize = new Sequelize(path);
 
 sequelize.authenticate().then(() => {
